e9557e8c2c42f0619050a70bbb5d5d42e23de935
Dennis Dollee <dennisdollee@gmail.com>
2017-01-19 19:23:56 +0000

    Try and catch for ValueErrors (Primers.checkInput)

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ class Primers():
         self.sequence = sequence
 
     def checkInput(self):
+        """ Checks the input sequence.
+            Removes every character that's NOT in CATG
+            Raises ValueError if condition is (not) met"""
+
         self.sequence = re.sub(r'[^CATG]', '', self.sequence.upper())
+        if not(20 <= len(self.sequence) <= 500):
+            raise ValueError("Sequentie moet tussen 20 en 500 lang zijn. "
+                             "Sequentie is nu {}: {:d} teken(s) lang."
+                             .format(self.sequence, len(self.sequence)))
         return self.sequence
 
 
 """ BELOW FOR TESTING PURPOSES ONLY """
-
+ask = input('seq? ')
 sq = "CCCTAAGTTTGATGAGTATAGAAATGGATCC"
 mt = """TTGATGAGTATAGAAATGGATCCACTCGTTATTCTCGGACGAGTGTTCAGTAATGAACCTCTGGAGAGAA
 CCATGTATATGATCGTTATCTGGGTTGGACTTCTGCTTTTAAGCCCAGATAACTGGCCTGAATATGTTAA
@@ -21,5 +29,8 @@ AAAACGATAAAGTGCGATCAGTAATTCAAAACCTTACAGAAGAGCAATCTATGGTTTTGTGCGCAGCCCT
 TAATGAAGGCAGGAAGTATGTGGTTACATCAAAACAATTCCCATACATTAGTGAGTTGATTGAGCTTGGT
 GTGTTGAACAAAACTTTTTCCCGATGGAATGGAAAGCATATATTATTCCCTATTGAGGATATTTACTGGA
 CTGAATTAGTTGCCAGCTATGATCCATATAATATTGAGATAAAGCCAAGGCCAATATCTAAGTAA"""
-_primer = Primers(sq)
-print(_primer.checkInput())
+_primer = Primers(ask)
+try:
+    print(_primer.checkInput())
+except ValueError as e:
+    print(e)
